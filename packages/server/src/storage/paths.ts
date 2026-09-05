3aef368720dcba8c5be40f43bd1329d8bd6e7ee1
Chris van der Loo <chris.vanderloo@icloud.com>
2026-09-05 14:01:38 -0400

    refactor: remove the local JSON user database

--- packages/server/src/storage/paths.ts
+++
@@ -1,5 +0,0 @@
-import { join } from "node:path"
-
-const dataDirectory = process.env["CRATE_DATA_DIR"] ?? join(process.cwd(), ".crate-data")
-
-export const dataPath = (name: string) => join(dataDirectory, name)
